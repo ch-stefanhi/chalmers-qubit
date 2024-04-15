1775edc2ce25a9a44bd021019352bd8a14740007
Pontus Wikståhl <pontus.wikstahl@gmail.com>
2024-04-15 10:00:43 +0200

    Upgraded the setup for the package

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 import setuptools
 
-setuptools.setup(
-    name="chalmers_qubit",
-    version="0.1",
-    author="Pontus Vikstål, Kamanasish Debnath, Shahnawaz Ahmed",
-    description="A model of the Chalmers device to be used with qutip-qip",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "qutip-qip",
-    ],
-    python_requires=">=3.9",
-)
+if __name__ == "__main__":
+    setuptools.setup()
