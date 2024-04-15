9422f3c34ca662e8c63b09658b25ba5ae75cef8a
Pontus Wikståhl <pontus.wikstahl@gmail.com>
2024-04-15 09:40:04 +0200

    added missing __init__ files

---
+++ chalmers_qubit/base/__init__.py
@@ -0,0 +1,4 @@
+from chalmers_qubit.base.processor import Processor
+from chalmers_qubit.base.noise import Noise
+from chalmers_qubit.base.operations import project_on_qubit
+from chalmers_qubit.base.gates import cczs
--- chalmers_qubit/sarimner/__init__.py
+++ chalmers_qubit/sarimner/__init__.py
@@ -1,8 +1,3 @@
-# -----------------------------------------------------------------------------
-# Load modules
-#
-
-# core
 from chalmers_qubit.sarimner.model import SarimnerModel
 from chalmers_qubit.sarimner.compiler import SarimnerCompiler
 from chalmers_qubit.sarimner.processor import SarimnerProcessor
